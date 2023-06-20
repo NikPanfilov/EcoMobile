c9f32afab5e2ada40bb2de2c327456802232ffc2
NikPanfilov <npanfilov2003@gmail.com>
2023-06-20 20:43:15 +0700

    Сделал основную логику для профиля.

--- app/src/main/java/com/startup/ecoapp/App.kt
+++ app/src/main/java/com/startup/ecoapp/App.kt
@@ -6,6 +6,7 @@ import com.startup.ecoapp.di.commentModule
 import com.startup.ecoapp.di.homeModule
 import com.startup.ecoapp.di.networkModule
 import com.startup.ecoapp.di.postModule
+import com.startup.ecoapp.di.profileModule
 import com.startup.ecoapp.di.reactionsModule
 import com.startup.ecoapp.di.signInModule
 import com.startup.ecoapp.di.signUpModule
@@ -49,6 +50,7 @@ class App : Application() {
 			modules(signUpModule)
 			modules(signInModule)
 			modules(homeModule)
+			modules(profileModule)
 		}
 	}
 }
